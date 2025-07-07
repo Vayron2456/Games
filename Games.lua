cbaf0784009c80dfb15f21d269b8a6cc2e59ee1e
AndrewV2 <andrewtran356@gmail.com>
2025-07-06 21:59:13 -0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,14 +1,7 @@
 local Supported = {
 
-    [7580655576] = "https://raw.githubusercontent.com/5gm/scripts/main/Pet%20Crystal%20simulatorui2.lua",
-    [7463504342] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Pet%20Incrementalui2.lua",
-    [7114126212] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Freekick%20simulator.lua",
-    [7541395924] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Build%20An%20Island.lua",
-    [7679508751] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Infinite%20Mining%20Incremental.lua",
-    [7799183193] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Steal%20a%20car.lua",
-    [7881089396] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Anime%20Rising.lua",
-    [7218065222] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/dig.lua",
-    
+    [537413528] = "https://raw.githubusercontent.com/Vayron2456/BuildaBoat/refs/heads/main/Build%20a%20Boat.lua",
+    [13864661000] = "https://raw.githubusercontent.com/Vayron2456/Break-In-Story-2/refs/heads/main/Break%20In%202.lua",
 
     -- Add more games below following the same format:
     -- [1234567890] = "https://raw.githubusercontent.com/username/repo/main/script.lua",

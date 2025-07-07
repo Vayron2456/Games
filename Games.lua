67ce11774836d522936dc34c0b108a691ec4447c
AndrewV2 <andrewtran356@gmail.com>
2025-07-06 22:07:23 -0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,7 +1,7 @@
 local Supported = {
 
-    [537413528] = "https://raw.githubusercontent.com/Vayron2456/BuildaBoat/refs/heads/main/Build%20a%20Boat.lua",
-    [13864661000] = "https://raw.githubusercontent.com/Vayron2456/Break-In-Story-2/refs/heads/main/Break%20In%202.lua",
+    [13864661000] = "https://raw.githubusercontent.com/Vayron2456/BuildaBoat/refs/heads/main/Build%20a%20Boat.lua",
+    [537413528] = "https://raw.githubusercontent.com/Vayron2456/Break-In-Story-2/refs/heads/main/Break%20In%202.lua",
 }
 
 local ID = game.GameId
